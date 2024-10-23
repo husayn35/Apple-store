2905318edd6750c611ea8a9bfbc0911bfb10becf
husayn35 <habibullayevhusayn@gmail.com>
2024-10-23 13:00:08 +0500

    tiny update

--- js/iphone.js
+++
@@ -1,6 +0,0 @@
-const video = document.getElementById("myVideo");
-
-video.addEventListener("ended", function () {
-  video.currentTime = 0; // Videoni boshidan boshlaydi
-  video.play(); // Videoni qayta ijro etadi
-});
