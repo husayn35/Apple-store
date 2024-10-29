8442bde2d1756145eb882ffa5fc63afe5a23bdf4
husayn35 <habibullayevhusayn@gmail.com>
2024-10-29 20:45:55 +0500

    V1.1

--- js/loading.js
+++ js/loading.js
@@ -1,19 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const images = document.querySelectorAll("img");
-  let loadedImages = 0;
+  if (window.location.pathname.includes("pages/iphone.html")) {
+    const images = document.querySelectorAll("img[data-src]");
+    let loadedImages = 0;
 
-  images.forEach((img) => {
-    img.addEventListener("load", updateProgress);
-    img.addEventListener("error", updateProgress); // Xato yuz bersa ham hisoblash uchun
-  });
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const img = entry.target;
 
-  function updateProgress() {
-    loadedImages++;
-    const progress = (loadedImages / images.length) * 100;
-    document.getElementById("progress-bar").style.width = progress + "%";
+          img.addEventListener("load", updateProgress);
+          img.addEventListener("error", updateProgress);
 
-    if (loadedImages === images.length) {
-      document.getElementById("loading-screen").style.display = "none";
+          img.src = img.getAttribute("data-src");
+          observer.unobserve(img);
+        }
+      });
+    });
+
+    images.forEach((img) => {
+      img.setAttribute("data-src", img.src);
+      img.src = "";
+      observer.observe(img);
+    });
+
+    function updateProgress() {
+      loadedImages++;
+      if (loadedImages === images.length) {
+        document.getElementById("loading-screen").style.display = "none";
+      }
     }
   }
 });
